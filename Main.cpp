b8bff36abd82c5f2c705a0968a6afbc52351e835
YukkuriB <yukkuri@foxmail.com>
2023-10-23 19:42:49 +0800

    实现了3d化

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,7 @@
 #include"VBO.h"
 #include"EBO.h"
 #include "shaderClass.h"
+#include "Camera.h"
 
 
 
@@ -24,14 +25,18 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	 
-	// 定义定点位置
+	// 设置窗口尺寸
+
+	const unsigned int width = 800;
+	const unsigned int height = 800;
 
 	GLfloat vertices[] =
 	{ //坐标                 /   色彩                 //
-		-0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,           0.0f, 0.0f,//下左
-		-0.5f,  0.5f, 0.0f,    0.0f, 1.0f, 0.0f,           0.0f, 1.0f, //上左
-		 0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f,           1.0f, 1.0f, //上右
-		 0.5f,  -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,           1.0f, 0.0f, //下右
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 
 	};
 	
@@ -39,13 +44,17 @@ int main()
 
 	GLuint indices[] =
 	{
-		0, 2, 1, //上半三角形
-		0, 3, 2, //下半三角形
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 	};
 
 	// 创建窗口，如果窗口出问题就以failed结束
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "HelloWorld", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "HelloWorld", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -59,11 +68,11 @@ int main()
 	gladLoadGL();
 
 	// 创建视图并以特定颜色刷新缓冲区
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	Shader shaderProgram("default.vert", "default.frag");
 
-
+	//创建、vao、vbo、ebo，绑定并绘制
 	VAO VAO1;
 	VAO1.Bind();
 
@@ -78,33 +87,35 @@ int main()
 	EBO1.Unbind();
 
 
-	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
 
 	// ——————————————————————————————贴图部分——————————————————————————————————
 	Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	popCat.texUnit(shaderProgram, "tex0", 0);
 
+	glEnable(GL_DEPTH_TEST);
 
-	//等需要关闭窗口再关闭窗口
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+	
+
+	//运行主函数
 	while (!glfwWindowShouldClose(window))
 	{
-		float timeValue = glfwGetTime();
-		float angle = timeValue * glm::radians(50.0f);  // 旋转速度为50度/秒
-		glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
+
 
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		//激活着色器程序
 		shaderProgram.Activate();
 
-		glUniform1f(uniID, 0.5f);
+		camera.Inputs(window);
+		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
+		
+		//绑定贴图
 		popCat.Bind();
-		//下面是关于旋转的自加程序
-		GLuint rotationLoc = glGetUniformLocation(shaderProgram.ID, "rotationMatrix");
-		glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, glm::value_ptr(rotation));
-
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
