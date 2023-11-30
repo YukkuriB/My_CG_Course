ce1a753bda46df8840308620b261512ba65a4bb7
YukkuriB <yukkuri@foxmail.com>
2023-11-30 22:04:40 +0800

    可以用imgui更改三角形，但看起来数据位置不太对

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,7 @@
+#include"imgui.h"
+#include"imgui_impl_glfw.h"
+#include"imgui_impl_opengl3.h"
+
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
@@ -11,7 +15,12 @@
 
 
 
-
+void updateVertexColors(GLfloat vertices[], const GLfloat color[3], int numVertices) {
+	for (int i = 0; i < numVertices; ++i) {
+		int colorIndex = i * 6 + 3; // 计算颜色在数组中的起始索引
+		std::copy(color, color + 3, &vertices[colorIndex]);
+	}
+}
 
 int main()
 {
@@ -22,22 +31,19 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	 
 	// 定义定点位置
+	float triangleColor[3] = { 1.0f, 1.0f, 1.0f }; // 白色
 
 	GLfloat vertices[] =
-	{ //坐标                                              /   色彩                 //
-		-0.5f,    -0.5f * float(sqrt(3)) / 3,       0.0f,     0.8f, 0.3f,  0.02f, //下左角
-		0.5f,     -0.5f * float(sqrt(3)) / 3,       0.0f,     0.8f, 0.3f,  0.02f, //
-		0.0f,      0.5f * float(sqrt(3)) * 2 / 3,   0.0f,     1.0f, 0.6f,  0.32f,
-		-0.5f / 2, 0.5f * float(sqrt(3)) / 6,       0.0f,     0.9f, 0.45f, 0.17f, //内左
-		0.5f / 2,  0.5f * float(sqrt(3)) / 6,       0.0f,     0.9f, 0.45f, 0.17f, //内右
-		0.0f,     -0.5f * float(sqrt(3)) / 3,       0.0f,     0.8f, 0.3f,  0.02f,//内下
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
 	};
 	
 	GLuint indices[] =
 	{
-		0, 3, 5, //下左
-		3, 2, 4, //下右三角形
-		5, 4, 1 //上三角形
+		0, 1, 2, 
+
 	};
 
 	// 创建窗口，如果窗口出问题就以failed结束
@@ -67,7 +73,7 @@ int main()
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 0, (void*)0);
 	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3* sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
@@ -78,6 +84,12 @@ int main()
 
 	glfwSwapBuffers(window);
 
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
 
 
 	//等需要关闭窗口再关闭窗口
@@ -90,20 +102,49 @@ int main()
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		//激活着色器程序
+
+	
+
 		shaderProgram.Activate();
 
-		glUniform1f(uniID, 0.5f);
+		glUniform3fv(glGetUniformLocation(shaderProgram.ID, "triangleColor"), 1, triangleColor);
 
 		//下面是关于旋转的自加程序
 		GLuint rotationLoc = glGetUniformLocation(shaderProgram.ID, "rotationMatrix");
 		glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, glm::value_ptr(rotation));
 
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		// 创建一个空的 ImGui 窗口
+		static GLfloat currentColor[3] = { 1.0f, 1.0f, 1.0f };
+		ImGui::Begin("Triangle Color Picker");
+		if (ImGui::ColorEdit3("Triangle Color", currentColor)) {
+			// 当颜色改变时更新顶点颜色
+			updateVertexColors(vertices, currentColor, 3); // 更新颜色
+
+			// 更新 VBO 数据
+			VBO1.Bind();
+			glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // 使用 glBufferSubData 而非 glBufferData
+			VBO1.Unbind();
+		}
+		ImGui::End();
+
+
+		// 渲染 ImGui 界面
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	VAO1.Delete();
 	VBO1.Delete();
