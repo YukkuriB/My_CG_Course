8286f7cc766871ba2e1e5251f89e7b3942239b9c
YukkuriB <yukkuri@foxmail.com>
2023-12-01 19:43:02 +0800

    已经实现正反四角和三角形旋转

--- Main.cpp
+++ Main.cpp
@@ -32,15 +32,20 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	 
-	// 定义顶点
+
 	float triangleColor[3] = { 1.0f, 1.0f, 1.0f }; // 白色
 	float rotationSpeed = -50.0f;
+	float rotationSpeed2 = -50.0f;
+	float translationSpeed = 0.5f;
+	glm::vec3 translation(0.0f, 0.0f, 0.0f);
+
 	float BgColor[4] = { 0.07f, 0.07f, 0.07f, 0.07f }; // 白色
+	// 定义顶点
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 1.0f, 1.0f, 1.0f, // 第一个顶点的位置和颜色
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 1.0f, 1.0f, 1.0f,   // 第二个顶点的位置和颜色
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 1.0f, 1.0f // 第三个顶点的位置和颜色
+		0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, // 第一个顶点的位置和颜色
+		0.7f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,   // 第二个顶点的位置和颜色
+		0.0f, 1.0f, 0.0f, 0.0f, 0.5f, 1.0f, 1.0f // 第三个顶点的位置和颜色
 	};
 	
 	GLuint indices[] =
@@ -49,9 +54,26 @@ int main()
 
 	};
 
+	GLfloat verticesSquare[] = {
+		// 第一个三角形
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.8f, 0.0f, // 左下角 (亮黄色)
+		 0.5f, -0.5f, 0.0f, 0.2f, 0.2f, 0.0f, // 右下角 (暗黄色)
+		 0.5f,  0.5f, 0.0f, 1.0f, 0.9f, 0.5f, // 右上角 (非常亮的黄色)
+
+		// 第二个三角形
+		 0.5f,  0.5f, 0.0f, 1.0f, 0.9f, 0.0f, // 右上角 (非常亮的黄色)
+		-0.5f,  0.5f, 0.0f, 0.2f, 0.2f, 0.0f, // 左上角 (较暗黄色)
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.8f, 0.5f  // 左下角 (亮黄色)
+	};
+
+	GLuint indicesSquare[] = {
+		0, 1, 2, // 第一个三角形
+		3, 4, 5  // 第二个三角形
+	};
+
 	// 创建窗口，如果窗口出问题就以failed结束
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "HelloWorld", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1080, 1080, "HelloWorld", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -65,7 +87,12 @@ int main()
 	gladLoadGL();
 
 	// 创建视图并以特定颜色刷新缓冲区
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, 1080, 1080);
+	
+	//宽屏投影矩阵
+	glm::mat4 projection;
+	projection = glm::ortho(0.0f, 1920.0f, 0.0f, 1080.0f);
+
 
 	Shader shaderProgram("default.vert", "default.frag");
 
@@ -73,15 +100,28 @@ int main()
 	VAO VAO1;
 	VAO1.Bind();
 
+
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
+
 
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
 	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
+	//创建方形
 
+	VAO VAO2;
+	VAO2.Bind();
+	VBO VBO2(verticesSquare, sizeof(verticesSquare));
+	EBO EBO2(indicesSquare, sizeof(indicesSquare));
+	VAO2.LinkAttrib(VBO2, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO2.LinkAttrib(VBO2, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+	VAO2.Unbind();
+	VBO2.Unbind();
+	EBO2.Unbind();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
@@ -99,9 +139,18 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		float timeValue = glfwGetTime();
+		//更新平移向量
+		translation.x += translationSpeed * timeValue;
+
 		float angle = timeValue * glm::radians(rotationSpeed);  // 旋转速度为50度/秒
 		glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
+		glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), translation);
+		glm::mat4 modelMatrix = translationMatrix * rotation;
 
+		//投影矩阵
+	
+
+		//刷新背景
 		glClearColor(0.07f, 0.07f, 0.07f, 0.07f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		//激活着色器程序
@@ -109,14 +158,25 @@ int main()
 	
 
 		shaderProgram.Activate();
+
 		glUniform3fv(glGetUniformLocation(shaderProgram.ID, "triangleColor"), 1, triangleColor);
 
-		//下面是关于旋转的自加程序
+		//旋转三角形
 		GLuint rotationLoc = glGetUniformLocation(shaderProgram.ID, "rotationMatrix");
 		glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, glm::value_ptr(rotation));
 
+
+		//绘制三角形
 		VAO1.Bind();
 		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		VAO1.Unbind();
+		//第二个矩阵
+		glm::mat4 reverseMatrix = glm::rotate(glm::mat4(1.0f), -angle, glm::vec3(0.0f, 0.0f, 1.0f));
+		glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, glm::value_ptr(reverseMatrix));
+		//绘制方形
+		VAO2.Bind();
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); 
+		VAO2.Unbind();
 
 
 		//imgui控制部分
@@ -130,20 +190,19 @@ int main()
 		if (ImGui::IsWindowHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Right)) {
 			ImGui::OpenPopup("Color Context Menu");
 		}
-
 		if (ImGui::BeginPopup("Color Context Menu")) {
 			//1弹出菜单项
 			if (ImGui::MenuItem("Red")) {
 				GLfloat red[3] = { 1.0f, 0.0f, 0.0f };
-				updateVertexColors(vertices, red, 3);
+				//updateVertexColors(vertices, red, 3);
 			}
 			if (ImGui::MenuItem("Green")) {
 				GLfloat green[3] = { 0.0f, 1.0f, 0.0f };
-				updateVertexColors(vertices, green, 3);
+				//updateVertexColors(vertices, green, 3);
 			}
 			if (ImGui::MenuItem("Blue")) {
 				GLfloat blue[3] = { 0.0f, 0.0f, 1.0f };
-				updateVertexColors(vertices, blue, 3);
+				//updateVertexColors(vertices, blue, 3);
 			}
 			//刷新vbo
 			VBO1.Bind();
@@ -153,15 +212,12 @@ int main()
 
 			ImGui::EndPopup();
 		}
-
 		//2更改旋转速度部分
 		ImGui::SliderFloat("Rotation Speed", &rotationSpeed, -10000.0f, 10000.0f); // 范围从0到100
-		
-
 		//3更改色彩部分
 		if (ImGui::ColorEdit3("Triangle Color", currentColor)) {
 			//如果颜色改变就更新颜色
-			updateVertexColors(vertices, currentColor, 3); // 更新颜色
+			//updateVertexColors(vertices, currentColor, 3); // 更新颜色
 
 			//刷新vbo
 			VBO1.Bind();
@@ -169,11 +225,7 @@ int main()
 			VBO1.Unbind();
 		}
 		ImGui::Text("Homework C1");
-
-		
 		ImGui::End();
-
-
 		// imgui界面渲染
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
